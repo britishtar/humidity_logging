8cb7ab265da817f0dda823f61fbe5a46264d9b94
britishtar <britishtar@yahoo.com>
2020-10-21 23:22:23 -0400

    fixed problem with device initialization

--- dht11-logging.py
+++ dht11-logging.py
@@ -5,6 +5,7 @@ import pytz
 import time
 import board
 import adafruit_dht
+import psutil
 
 # Set variables appropriately:
 logfile = "/home/pi/dhtlogs/DHT-{}.log".format(dt.now().strftime("%Y-%m-%d"))
@@ -15,16 +16,31 @@ sampling_interval = 30.0 # number of seconds to wait betweek read attempts
 timestamp = timezone.localize(dt.now()).strftime("%Y-%m-%d-%H:%M:%S") 
 with open(logfile, "a") as f:
     f.write("Initialized logging script at {}".format(timestamp))
+    f.write("\n")
 
 # Initial the dht device, with data pin connected to:
-dhtDevice = adafruit_dht.DHT11(board.D4)
+flag = True
+while flag:
+    try:
+        print("trying to initialize device...")
+        dhtDevice = adafruit_dht.DHT11(board.D4)
+        dhtDevice.temperature 
+        print("successfully initialized DHT11")
+    except:
+        for p in psutil.process_iter():
+            if p.name()[:8] == 'libgpiod':
+                print("killing {} (pid: {})".format(p.name(), p.pid))
+                p.kill()
+        time.sleep(5)
+        continue
+    flag = False
 
 # you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
 # This may be necessary on a Linux single board computer like the Raspberry Pi,
 # but it will not work in CircuitPython.
 # dhtDevice = adafruit_dht.DHT22(board.D18, use_pulseio=False)
 
-sampling_interval = 30.0 # number of seconds to wait betweek read attempts
+print("entering logging mode, sampling interval set to: {} seconds".format(sampling_interval))
 
 while True:
     logfile = "/home/pi/dhtlogs/DHT-{}.log".format(dt.now().strftime("%Y-%m-%d"))
